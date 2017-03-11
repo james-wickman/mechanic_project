088c8d6cb02b9a895319f82d17300c938463d259
nauticalnova <nauticalnova@hotmail.com>
2017-03-11 15:26:03 -0700

    fixed restfullrouting

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,8 +1,8 @@
 class Appointment < ApplicationRecord
 
 	validates :date,  :presence => true
-	validates :hour,  :presence => true,
-                	  :uniqueness => {:scope => :date}
+	validates :hour,  :presence => true
+
 
     has_one :job
     belongs_to :mechanic
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -6,7 +6,7 @@ class Mechanic < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-    geocoded_by :full_street_address   # can also be an IP address
+  geocoded_by :full_street_address   # can also be an IP address
 	after_validation :geocode         # auto-fetch coordinates
 
 	def full_street_address
